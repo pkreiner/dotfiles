291395f95e406b867b954d9d7af66279e3cb92b5
Paul Kreiner <pkreiner@alum.mit.edu>
2021-09-25 12:41:25 -0400

    added position-in-paragraph data to tweet mode

--- .emacs.d/pk/tweet-mode.el
+++ .emacs.d/pk/tweet-mode.el
@@ -20,26 +20,24 @@
 	       (butlast mode-line-format))))))
 
 
-;; for debugging:
-;; (add-hook 'post-command-hook 'tweet-mode-update)
-;; (remove-hook 'post-command-hook 'tweet-mode-update)
-
-
 (defun tweet-mode-update ()
-  (setq tweet-mode-char-count (pk/count-chars-current-paragraph))
-  (setq tweet-mode-string (concat "    " (number-to-string tweet-mode-char-count) "/280")))
+  ;; (message "updating")
+  (setq tweet-mode-string (create-tweet-mode-string))
+  (force-mode-line-update))
 
+(defun create-tweet-mode-string ()
+  (let* ((end (end-of-paragraph))
+	 (beg (beginning-of-paragraph))
+	 (pos (point))
+	 (par-length (- end beg))
+	 (pos-in-par (- pos beg)))
+    (concat " " (number-to-string par-length) "/280"
+	    " (" (number-to-string pos-in-par) ")"
+	    )))
 
-;; count number of chars in paragraph
-(defun pk/count-chars-current-paragraph ()
-  "Count the number of characters in the paragraph that the mark is in."
-  (interactive)
-  (- (end-of-paragraph) (beginning-of-paragraph)))
-  
 ;; Assumes two newlines before paragraph,
 ;; may be 1-2 chars off otherwise.
 (defun beginning-of-paragraph ()
-  (interactive)
   (save-excursion
     (search-backward "\n\n" nil "no-error")
     (+ (point) 2)))
@@ -47,8 +45,6 @@
 ;; Assumes two newlines after paragraph,
 ;; may be 1-2 chars off otherwise
 (defun end-of-paragraph ()
-  (interactive)
   (save-excursion
     (search-forward "\n\n" nil "no-error")
     (- (point) 2)))
-
